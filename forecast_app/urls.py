c8e06e75aaea144880b72719888c535fe342adc7
Adam Fairlie <2461352f@student.gla.ac.uk>
2021-08-10 20:16:36 +0100

    Added URLs and views for all non-JSON pages

--- forecast_app/urls.py
+++ forecast_app/urls.py
@@ -5,4 +5,13 @@ app_name = "forecast_app"
 
 urlpatterns = [
     path("", views.homepage, name="home"),
+    path("login/", views.login, name="login"),
+    path("register/", views.register, name="register"),
+    path("player/<int:player_id>/", views.player, name="player"),
+    path("league/join/", views.league_join, name="league_join"),
+    path("league/<int:league_id>/", views.league, name="league"),
+    path("tournaments/", views.tournaments, name="tournaments"),
+    path("tournaments/<slug:tour_slug>/", views.tournament_overview, name="tournament_overview"),
+    path("tournaments/<slug:tour_slug>/enter/", views.tournament_enter, name="tournament_enter"),
+    path("tournaments/<slug:tour_slug>/predict/", views.tournament_predict, name="tournament_predict"),
 ]
--- forecast_app/views.py
+++ forecast_app/views.py
@@ -6,3 +6,64 @@ def homepage(request):
     # Create response render
     response = render(request, 'forecast_app/home.html')
     return response
+
+
+def login(request):
+    # Create response render
+    response = render(request, 'forecast_app/login.html')
+    return response
+
+
+def register(request):
+    # Create response render
+    response = render(request, 'forecast_app/register.html')
+    return response
+
+
+def player(request, player_id):
+    context_dict = {"player_id": player_id}
+    # Create response render
+    response = render(request, 'forecast_app/player.html', context=context_dict)
+    return response
+
+
+def league_join(request):
+    # Create response render
+    response = render(request, 'forecast_app/league_join.html')
+    return response
+
+
+def league(request, league_id):
+    context_dict = {"league_id": league_id}
+    # Create response render
+    response = render(request, 'forecast_app/league.html', context=context_dict)
+    return response
+
+
+def tournaments(request):
+    # Create response render
+    response = render(request, 'forecast_app/tournaments.html')
+    return response
+
+
+def tournament_overview(request, tour_slug):
+    context_dict = {"tour_slug": tour_slug}
+    # Create response render
+    response = render(request, 'forecast_app/tournament_page.html', context=context_dict)
+    return response
+
+
+def tournament_enter(request, tour_slug):
+    context_dict = {"tour_slug": tour_slug}
+    # Create response render
+    response = render(request, 'forecast_app/tournament_enter.html', context=context_dict)
+    return response
+
+
+def tournament_predict(request, tour_slug):
+    context_dict = {"tour_slug": tour_slug}
+    # Create response render
+    response = render(request, 'forecast_app/tournament_predict.html', context=context_dict)
+    return response
+
+
